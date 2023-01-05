77909f3daf138883370946ca6247a8abf48f1191
Kairat Doshekenov <kayrat@bk.ru>
2023-01-05 10:48:51 +0600

    fix null client on candidate

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-import { readFile, truncate } from "fs"
-import { WebSocketServer } from "ws"
-import { createServer } from "http"
+import { readFile, truncate } from "fs";
+import { WebSocketServer } from "ws";
+import { createServer } from "http";
 import https from "https";
 
-const OFFER = 'offer'
-const ANSWER = 'answer'
-const CANDIDATE='candidate'
-const PEER = 'peer'
-const BYE = 'bye'
-const NEW = 'new'
-const NEXT = 'next'
-const REPORT = 'report'
+const OFFER = "offer";
+const ANSWER = "answer";
+const CANDIDATE = "candidate";
+const PEER = "peer";
+const BYE = "bye";
+const NEW = "new";
+const NEXT = "next";
+const REPORT = "report";
 
 class CallHandler {
   constructor() {
-    this.wss = null
+    this.wss = null;
     this.ws = null;
     this.clients = new Set();
     this.server = null;
@@ -66,16 +66,19 @@ class CallHandler {
   _getFreePeer = (client_self) => {
     console.log("get free peer from " + this.clients.size);
     for (const client of this.clients) {
-      if(client_self.id === client.id) continue;
+      if (client_self.id === client.id) continue;
       console.log(`id=${client.id}, busy=${client.busy}`);
-      if (client.busy === false && !client_self.blockedPeers.has(client.id) && !client.blockedPeers.has(client_self.id)) {
+      if (
+        client.busy === false &&
+        !client_self.blockedPeers.has(client.id) &&
+        !client.blockedPeers.has(client_self.id)
+      ) {
         client.busy = true;
-        return { type: PEER, id:client.id, name: client.name}
+        return { type: PEER, id: client.id, name: client.name };
       }
     }
     return null;
   };
-
 
   onConnection = (client_self) => {
     this.clients.add(client_self);
@@ -88,8 +91,8 @@ class CallHandler {
         client.send(JSON.stringify({ type: "bye" }));
         client.blockedPeers.add(client_self.id);
       }
-        const removed = this.clients.delete(client_self);
-        console.log('on close id:' + client_self.id+'; removed:'+removed);
+      const removed = this.clients.delete(client_self);
+      console.log("on close id:" + client_self.id + "; removed:" + removed);
     });
 
     client_self.on("message", (message) => {
@@ -111,7 +114,7 @@ class CallHandler {
           message.type == OFFER ||
           message.type == ANSWER ||
           message.type == BYE ||
-          message.type==REPORT
+          message.type == REPORT
         ) {
           console.log(
             message.type + " from " + client_self.id + " to: " + message.to
@@ -125,49 +128,51 @@ class CallHandler {
         case REPORT:
           this._findAndSend({ type: REPORT }, message.to);
           break;
-        case NEXT: this._getPeerAndSend(client_self)
-        break;
+        case NEXT:
+          this._getPeerAndSend(client_self);
+          break;
         case NEW:
-          this._checkDeadClient(this.clients,message.id)
-          client_self.id = "" + message.id
-          client_self.busy = true
-          client_self.name = message.name
-          client_self.blockedPeers = new Set(message.blockedPeers)
-          this._getPeerAndSend(client_self)
+          this._checkDeadClient(this.clients, message.id);
+          client_self.id = "" + message.id;
+          client_self.busy = true;
+          client_self.name = message.name;
+          client_self.blockedPeers = new Set(message.blockedPeers);
+          this._getPeerAndSend(client_self);
           break;
         case BYE:
-          client = this._getById(message.to)
-          client_self.peerId = undefined
-          client.peerId = undefined
-          client.blockedPeers.add(client_self.id)
-          client.send(JSON.stringify({ type: BYE }))
-          this._addToBlockedAndFindNewPeer(client_self, client.id)
+          client = this._getById(message.to);
+          client_self.peerId = undefined;
+          client.peerId = undefined;
+          client.blockedPeers.add(client_self.id);
+          client.send(JSON.stringify({ type: BYE }));
+          this._addToBlockedAndFindNewPeer(client_self, client.id);
           break;
         case OFFER:
           client = this._getById(message.to);
           if (client != null) {
-            message.from = client_self.id
-            message.to=undefined  
+            message.from = client_self.id;
+            message.to = undefined;
             client.send(JSON.stringify(message));
             client_self.busy = true;
             client.busy = true;
           } else client_self.busy = false;
           break;
         case ANSWER:
-          client = this._getById(message.to)
+          client = this._getById(message.to);
           if (client != null) {
-            message.to=undefined
-            message.from=client_self.id
-            client.send(JSON.stringify(message))
-            client_self.peerId = client.id
-            client.peerId = client_self.id
-          } else client_self.busy = false
-          break
+            message.to = undefined;
+            message.from = client_self.id;
+            client.send(JSON.stringify(message));
+            client_self.peerId = client.id;
+            client.peerId = client_self.id;
+          } else client_self.busy = false;
+          break;
         case CANDIDATE:
-          client = this._getById(message.to)
-          message.to=undefined
-          message.from=client_self.id
-          client.send(JSON.stringify(message))
+          client = this._getById(message.to);
+          if (client == null) return;
+          message.to = undefined;
+          message.from = client_self.id;
+          client.send(JSON.stringify(message));
           break;
         case "keepalive":
           client_self.send(JSON.stringify({ type: "keepalive" }));
@@ -190,19 +195,19 @@ class CallHandler {
     return null;
   }
 
-  _getPeerAndSend(client_self){
+  _getPeerAndSend(client_self) {
     const peer = this._getFreePeer(client_self);
-          if (peer == null) {
-            client_self.busy = false;
-            client_self.peerId = undefined;
-          }else client_self.send(JSON.stringify(peer));
+    if (peer == null) {
+      client_self.busy = false;
+      client_self.peerId = undefined;
+    } else client_self.send(JSON.stringify(peer));
   }
 
-  _checkDeadClient(clients,id) {
-    const client=this._getById(id)
-    if(client!=null){
-      clients.delete(client)
-      console.log('removed dead client:'+id)
+  _checkDeadClient(clients, id) {
+    const client = this._getById(id);
+    if (client != null) {
+      clients.delete(client);
+      console.log("removed dead client:" + id);
     }
   }
 
